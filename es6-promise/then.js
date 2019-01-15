ec4319ecb5a82905ed7d266d1867aa8a9d6e7a6b
luoy <luoy@schbrain.com>
2019-01-15 21:14:33 +0800

    es6 promise

--- es6-promise/then.js
+++ es6-promise/then.js
@@ -1,21 +1,32 @@
 import {
-    invokeCallback,
-    subscribe,
-    FULFILLED,
-    REJECTED,
-    noop,
-    makePromise,
-    PROMISE_ID
-} from './-internal.js'
-console.log(PROMISE_ID,'w')
+  invokeCallback,
+  subscribe,
+  FULFILLED,
+  REJECTED,
+  noop,
+  makePromise,
+  PROMISE_ID
+} from './-internal.js';
+
 import { asap } from './asap.js';
 
 export default function then(onFulfillment, onRejection) {
-    const parent = this
+  const parent = this;
 
-    const child = new this.constructor(noop);
+  const child = new this.constructor(noop);
 
-    if(child[PROMISE_ID] === undefined){
+  if (child[PROMISE_ID] === undefined) {
+    makePromise(child);
+  }
 
-    }
+  const { _state } = parent;
+
+  if (_state) {
+    const callback = arguments[_state - 1];
+    asap(() => invokeCallback(_state, child, callback, parent._result));
+  } else {
+    subscribe(parent, child, onFulfillment, onRejection);
+  }
+
+  return child;
 }
